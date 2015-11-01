c26dad8311c146f93cf7e24b0feec4a010ef5b95
julesdc <julesdc@hotmail.com>
2015-11-01 11:44:40 -0500

    Update
    I finished the program - it needed some design changes which were handled are now included.

--- FileManager.java
+++ FileManager.java
@@ -1,15 +1,12 @@
-package programming.task.pkg3.madlib;
+package HandIn;
 
 /*
  * Author: Jules de Courtenay
- * Description: A class which imports the File, FileReader, PrintWriter
+ * Description: A class which is constructed by four file paths and produces a mad-lib sentence. 
+ * The sentence can be written to a text file.
  * Version, Date: V 1.0, 12/10/15
  */
 
-/**
- *
- * @author Jules
- */
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Random;
@@ -17,111 +14,148 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class FileManager {
-
-    private final String sentence, noun, verb;
+    
+    //instance variables
+    private final String sentence, noun, verb, output;
     private final CharSequence percent = "%", hash = "#";
-//constructor
-    public FileManager(String sentence, String noun, String verb){
+    
+    //constructor
+    public FileManager(String sentence, String noun, String verb, String output){
         this.sentence = sentence;
         this.noun = noun;
         this.verb = verb;
+        this.output = output;
     }
     
-    //method to return the final sentence with random verbs and nouns
-    public String createFinalSentence(){
+    public String createFinalSentence() {
+        /*
+        the action method of this class. replaces the sentence from the sentence file with
+        the finalized mad-lib sentence. Every time there is an error in any method they will return null,
+        the null value will come up the hierarchy to this method where it is finally handled with the
+        return statement
+        */
         String finalSentence;
         if(getInitialSentence() != null){
             finalSentence = getInitialSentence();            
-            finalSentence = fitNoun(finalSentence);
-            finalSentence = fitVerb(finalSentence);
+            finalSentence = fitWord(finalSentence, "#", noun, "noun");
+            finalSentence = fitWord(finalSentence, "%", verb, "verb");
             return finalSentence;
         }else{
             return "An error has occured with input. Please try again.";
         }
     }
     
-    //repalces the % in the sentence with a random verb
-    public String fitVerb(String sentence){
-        return sentence.replaceAll("[%]", getRandomWord(readVerbFile()));
+    public String fitWord(String sentence, String character, String path, String name){
+        /*
+        replaces all instances of "regex" in "sentence" with random words taken from the 
+        file at "path", producing an error message of "name"
+        */ 
+        String newStr[] = sentence.split(character);
+        String returnString = newStr[0];
+        for(int i = 1; i < newStr.length; i++){
+            returnString += getRandomWord(readWordFile(path,name)) + newStr[i];
+        }
+        return returnString;
     }
     
-    //replaces the # in the sentence with a random noun
-    public String fitNoun(String sentence){
-        return sentence.replaceAll("[#]", getRandomWord(readNounFile()));
-    }
-    
-    //returns a word from the specified noun or verb file
-    public String getRandomWord(FileReader fr){
-        ArrayList<String> words = new ArrayList<>();        
-        Scanner s = new Scanner(fr);
-        while (s.hasNextLine()){
-            words.add(s.nextLine());            
-        }     
-        int rnd = new Random().nextInt(words.size());
-        return words.get(rnd);
+    public String getRandomWord(File f){
+        /*
+        picks one random word from the specfied file and returns it.
+        */
+        ArrayList<String> words = new ArrayList<>(); 
+        try{
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()){
+                words.add(s.nextLine());            
+            }     
+            int rnd = new Random().nextInt(words.size());
+            s.close();
+            return words.get(rnd);
+        }catch(FileNotFoundException e){
+            //The word file cannot be found so can't make a scanner of it.
+            JOptionPane.showMessageDialog(null,  "Can't read word file. Error 2006", "Error!", JOptionPane.ERROR_MESSAGE);
+            return null;
+        }
     }
         
     //method which returns the sentence in the file entered by the user (String sentence). It will contain a # and a %
     public String getInitialSentence(){
-        if (fileRead(sentence) != null){
-            if (sentence.contains(percent) && sentence.contains(hash)){
-                FileReader fr = fileRead(sentence);
-                Scanner s = new Scanner(fr);
-                return s.nextLine();
-            }else{
-                JOptionPane.showMessageDialog(null,  "Sentence does not contain #s or %s. Error 2005", "Error!", JOptionPane.ERROR_MESSAGE);
-                return null;
+        /*
+        returns a line of text taken from the sentence file (see constructor).
+        this sentence is checked to contain both # and % so that it can be a proper mad-lib sentence
+        */
+        File sentenceFile = readFile(sentence);
+        if (sentenceFile != null){
+            try{
+                Scanner s = new Scanner(sentenceFile);
+                String sent = s.nextLine();
+                if (sent.contains(percent) || sent.contains(hash)){
+                    s.close();
+                    return sent;
+                }else{
+                    //doesn't contain # or %
+                    JOptionPane.showMessageDialog(null,  "Sentence does not contain #s or %s. Error 2005", "Error!", JOptionPane.ERROR_MESSAGE);
+                    s.close();
+                    return null;
+                }
+            }catch(FileNotFoundException e){
+                //can't find the sentence file
+                JOptionPane.showMessageDialog(null,  "Can't read sentence file. Error 2004-a", "Error!", JOptionPane.ERROR_MESSAGE);
+                return null;  
             }
         }else{
-            JOptionPane.showMessageDialog(null,  "Can't read sentence file. Error 2004", "Error!", JOptionPane.ERROR_MESSAGE);
+            //see readFile() method and find "return null;"
+            JOptionPane.showMessageDialog(null,  "Can't read sentence file. Error 2004-b", "Error!", JOptionPane.ERROR_MESSAGE);
             return null;   
         }
     }
     
-    //returns the entire string of the noun file
-    public FileReader readNounFile(){
-        if (fileRead(noun) != null){
-            return fileRead(noun);
+    public File readWordFile(String path, String name){
+        /*
+        returns a file object if the file exists at the specified path. This will be used for the noun or verb files
+        */
+        if (readFile(path) != null){
+            return readFile(path);
         }else{
-            JOptionPane.showMessageDialog(null,  "Can't read noun file. Error 2003", "Error!", JOptionPane.ERROR_MESSAGE);
+            //tells the user which file was input incorrectly
+            JOptionPane.showMessageDialog(null,  "Can't read " + name + " file. Error 2003", "Error!", JOptionPane.ERROR_MESSAGE);
             return null;
         }
     }
     
-    //returns the entire string of the verb file
-    public FileReader readVerbFile(){
-        if (fileRead(verb) != null){
-            return fileRead(verb);
-        }else{
-            JOptionPane.showMessageDialog(null,  "Can't read verb file. Error 2002", "Error!", JOptionPane.ERROR_MESSAGE);
-            return null;
-        }
-    }
-    
-    //returns a fileReader object of the specifed file
-    public FileReader fileRead(String s){
+    public File readFile(String s){
+        /*
+        returns a File object of the file at the specified file path
+        */
         File f = new File(s);
         try{
-            FileReader fr = new FileReader(f);
-            if (new Scanner(fr).hasNextLine()){
-                return fr;
+            Scanner fileScanner = new Scanner(f);
+            if (fileScanner.hasNextLine()){
+                fileScanner.close();
+                return f;
             }else{
+                //the file is empty
+                fileScanner.close();
                 JOptionPane.showMessageDialog(null,  "File Empty. Error 2000", "Error!", JOptionPane.ERROR_MESSAGE);
                 return null;
             }
         }catch(FileNotFoundException e){
+            //can't find the file
             return null;
         }
     }
     
-    //writes a specified string to a specifed file. Writing appends and does not use flush
-    public void fileWrite(String path, String write){
+    public void fileWrite(String write){
+        /*
+        writes a specified string to a specifed file. Writing appends and does not use flush    
+        */
         try{
-            PrintWriter pr = new PrintWriter(new FileWriter(new File(path), true), false);
+            PrintWriter pr = new PrintWriter(new FileWriter(new File(output), true), false);
             pr.println(write);
             pr.close();
         }catch(IOException e){
-            System.out.println("File not found. Error 2001.");
+            //thrown by printWriter object instantiation and .close() method
+            System.out.println("Error writing to file. Error 2001.");
         }
         
     }
